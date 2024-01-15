4bcca76b01418846aba57481c7a3b5f2015497dc
SafiHZN <safihazzanyl@gmail.com>
2024-01-15 17:46:59 +0200

    modified:   (Components)/RegisterPage.tsx 	modified:   (Components)/SignInPage.tsx

--- (Components)/RegisterPage.tsx
+++ (Components)/RegisterPage.tsx
@@ -1,12 +1,12 @@
 import React, { FC, ReactElement, useState } from "react";
-import { Button, StyleSheet, TextInput } from "react-native";
+import { Button, StyleSheet, TextInput, View } from "react-native";
 
 const RegisterPage = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
 
   return (
-    <>
+    <View style={styles.container}>
       <TextInput
         style={styles.input}
         value={username}
@@ -22,15 +22,26 @@ const RegisterPage = () => {
         onChangeText={(text) => setPassword(text)}
       />
       <Button title={"Create Account"} onPress={() => {}} />
-    </>
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
   input: {
-    height: 40,
+    padding: 15,
+    width: "90%",
+    height: 80,
+    fontSize: 35,
     marginBottom: 10,
-    backgroundColor: "#fff",
+    borderRadius: 10,
+    backgroundColor: "#1ea4fc",
+  },
+
+  container: {
+    alignItems: "center",
+    height: "100%",
+    width: "100%",
+    justifyContent: "center",
   },
 });
 
